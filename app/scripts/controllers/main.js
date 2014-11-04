6730ca8c5874b3a4a367d00e3a9d98d433828a3d
Brian Yang <mail@brianyang.com>
2014-11-03 23:49:48 -0500

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('unfilteredAngularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
